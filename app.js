bfb69c200d48f7b307d96b4fe3e7a4ec54353f99
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-03-15 08:43:20 +0100

    Add event listener on click or keypress

--- app.js
+++ app.js
@@ -7,19 +7,33 @@ var budgetController = (function () {
 //UI CONTROLLER
 var UIController = (function () {
 
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            }
+        }
+    }
+
 })();
 
 
 //GLOBAL APP CONTROLLER    
 var controller = (function (budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function () {
+    function addItem() {
+        
+        var getInput = UICtrl.getInput();
+        console.log(getInput);
+    }
 
-    });
+    document.querySelector('.add__btn').addEventListener('click', addItem);
 
     document.addEventListener('keypress', function (e) {
         if (e.keyCode === 13 || e.which === 13) {
-
+            addItem();
         }
     });
 
