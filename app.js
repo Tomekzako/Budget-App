c9a50d285db406c5ad07e78c3454eec6d669926d
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-03-27 09:19:48 +0200

    Add calculateBudget method

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ var budgetController = (function () {
         this.value = value;
     }
 
+
+    var calculateTotal = function (type) {
+        var sum = 0;
+        data.allItems[type].forEach(function (cur) {
+            sum = sum + cur.value;
+        });
+
+        data.totals[type] = sum;
+    }
+
     var data = {
         allItems: {
             exp: [],
@@ -21,7 +31,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -44,6 +56,29 @@ var budgetController = (function () {
 
             return newItem;
         },
+
+        calculateBudget: function () {
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            data.budget = data.totals.inc - data.totals.exp;
+
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+
+        },
+
+        getBudget: function () {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        }
     };
 
 })();
@@ -127,6 +162,17 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     };
 
+
+    var updateBudget = function () {
+
+        budgetCtrl.calculateBudget();
+
+        var budget = budgetCtrl.getBudget();
+
+        console.log(budget);
+    };
+
+
     function addItem() {
         var getInput, newItem;
 
@@ -140,6 +186,8 @@ var controller = (function (budgetCtrl, UICtrl) {
             UICtrl.addListItem(newItem, getInput.type);
 
             UICtrl.clearInputs();
+
+            updateBudget();
         }
     };
 
